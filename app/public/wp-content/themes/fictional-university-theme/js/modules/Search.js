d4df932859fe8f3af42523378e74498a07260aa5
DalatCoder <hieuntctk42@gmail.com>
2021-05-29 21:02:17 +0700

    Spinner 🐹

--- app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -1,67 +1,97 @@
-import $ from 'jquery'
+import $ from "jquery";
 
 class Search {
   // 1. describe and initiate our object
   constructor() {
-    this.openButton = $('.js-search-trigger')
-    this.closeButton = $('.search-overlay__close')
-    this.searchOverlay = $('.search-overlay')
-    this.searchInput = $('#search-term')
+    this.openButton = $(".js-search-trigger");
+    this.closeButton = $(".search-overlay__close");
+    this.searchOverlay = $(".search-overlay");
+    this.searchInput = $("#search-term");
+    this.resultsDiv = $("#search-overlay__results");
 
-    this.isOverlayOpen = false
+    this.isOverlayOpen = false;
+    this.isSpinnerVisible = false;
 
-    this.typingTimer
+    this.typingTimer;
+
+    this.previousValue = "";
 
     this.events();
   }
 
   // 2. events
   events() {
-    this.openButton.on('click', this.openOverlay.bind(this))
-    this.closeButton.on('click', this.closeOverlay.bind(this))
-    this.searchInput.on('input', this.typingLogic.bind(this))
+    this.openButton.on("click", this.openOverlay.bind(this));
+    this.closeButton.on("click", this.closeOverlay.bind(this));
+    this.searchInput.on("input", this.typingLogic.bind(this));
 
     // ESC to close overlay
-    $(document).on('keydown', this.keyPressDispatcher.bind(this))
+    $(document).on("keydown", this.keyPressDispatcher.bind(this));
   }
 
   // 3. methods
   openOverlay() {
-    this.searchOverlay.addClass('search-overlay--active')
+    this.searchOverlay.addClass("search-overlay--active");
     this.isOverlayOpen = true;
 
     // Remove scroll bar
-    $('body').addClass('body-no-scroll')
+    $("body").addClass("body-no-scroll");
   }
 
   closeOverlay() {
-    this.searchOverlay.removeClass('search-overlay--active')
+    this.searchOverlay.removeClass("search-overlay--active");
     this.isOverlayOpen = false;
 
     // Remove scroll bar
-    $('body').removeClass('body-no-scroll')
+    $("body").removeClass("body-no-scroll");
   }
 
   keyPressDispatcher(event) {
     var code = event.keyCode;
 
     // s key
-    if (code == 83 && !this.isOverlayOpen) {
-      this.openOverlay()
+    if (
+      code == 83 &&
+      !this.isOverlayOpen &&
+      !$("input, textarea").is(":focus")
+    ) {
+      this.openOverlay();
     }
 
     // esc key
     if (code == 27 && this.isOverlayOpen) {
-      this.closeOverlay()
+      this.closeOverlay();
     }
   }
 
   typingLogic() {
-    clearTimeout(this.typingTimer)
-    this.typingTimer = setTimeout(() => {
-      console.log('timeout test')
-    }, 2000);
+    /**
+     * Nếu người dùng nhập các phím như: mũi tên, ... thì không cần chạy các event bên dưới
+     */
+    if (this.searchInput.val() == this.previousValue) {
+      return;
+    }
+
+    clearTimeout(this.typingTimer);
+
+    if (this.searchInput.val()) {
+      if (!this.isSpinnerVisible) {
+        this.resultsDiv.html(`<div class="spinner-loader"></div>`);
+        this.isSpinnerVisible = true;
+      }
+
+      this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+      this.previousValue = this.searchInput.val();
+    } else {
+      this.resultsDiv.html(``);
+      this.isSpinnerVisible = false;
+    }
+  }
+
+  getResults() {
+    this.resultsDiv.html("");
+    this.isSpinnerVisible = false;
   }
 }
 
-export default Search
+export default Search;
