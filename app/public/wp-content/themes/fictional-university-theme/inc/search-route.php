5f4dc6d34aa626f26fd2758d04b659ed21e0d8b8
DalatCoder <hieuntctk42@gmail.com>
2021-05-30 15:28:40 +0700

    Conplete advanced live search 🦀

--- app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
+++ app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -51,6 +51,19 @@ function universitySearchResults($data)
                     'title' => get_the_title(),
                     'permalink' => get_the_permalink()
                 ]);
+
+                // Related campuses
+                $relatedCampuses = get_field('related_campus');
+
+                if (isset($relatedCampuses)) {
+                    foreach ($relatedCampuses as $campus) {
+                        array_push($results['campuses'], [
+                            'title' => get_the_title($campus),
+                            'permalink' => get_the_permalink($campus)
+                        ]);
+                    }
+                }
+
                 break;
 
             case 'campus':
@@ -103,7 +116,7 @@ function universitySearchResults($data)
         }
 
         $programRelationshipQuery = new WP_Query([
-            'post_type' => 'professor',
+            'post_type' => ['professor', 'event'],
             'meta_query' => [
                 'relation' => 'OR',
                 $filters
@@ -121,9 +134,28 @@ function universitySearchResults($data)
                     'image' => get_the_post_thumbnail_url(get_post(), 'professorLandscape')
                 ]);
             }
+
+            if (get_post_type() === 'event') {
+                $eventDate = new DateTime(get_field('event_date'));
+                $description = null;
+
+                if (has_excerpt())
+                    $description = get_the_excerpt();
+                else
+                    $description = wp_trim_words(get_the_content(), 18);
+
+                array_push($results['events'], [
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink(),
+                    'month' => $eventDate->format('M'),
+                    'day' => $eventDate->format('d'),
+                    'description' => $description
+                ]);
+            }
         }
 
         $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+        $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
     }
 
     return $results;
